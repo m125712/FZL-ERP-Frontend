d6d5151d9d68448f1cdb95ce799efe347b732da4
alifbinhossain <alifahmed47@gmail.com>
2024-08-19 21:48:59 +0600

    style: :art: modified styles to store/receive

---
+++ src/ui/Others/Breadcrumbs.jsx
@@ -0,0 +1,44 @@
+import { RightArrow } from '@/assets/icons';
+import cn from '@/lib/cn';
+import { Link } from 'react-router-dom';
+
+const Breadcrumbs = ({ items }) => {
+	return (
+		<div className='flex gap-1'>
+			<div className='flex items-center'>
+				<Link to='/dashboard' className=''>
+					Dashboard
+				</Link>
+				{items?.length > 0 && (
+					<span className='ml-1'>
+						<RightArrow />
+					</span>
+				)}
+			</div>
+			{items.map((item, index) => (
+				<div
+					key={index}
+					className={cn(
+						'flex items-center',
+						index === items?.length - 1 && 'font-medium'
+					)}>
+					{item.isDisabled ? (
+						<span className='opacity-00'>{item?.label}</span>
+					) : (
+						<Link to={item.href} className=''>
+							{item.label}
+						</Link>
+					)}
+
+					{index !== items?.length - 1 && (
+						<span className={cn('ml-1')}>
+							<RightArrow />
+						</span>
+					)}
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Breadcrumbs;

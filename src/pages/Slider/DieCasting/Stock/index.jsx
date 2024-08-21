8ee185d5c34cafe68d0ca1f78bbdcedf63e182b2
alifbinhossain <alifahmed47@gmail.com>
2024-08-21 15:52:26 +0600

    ci: :sparkles: removed columns from the /slider/stock

--- src/pages/Slider/DieCasting/Stock/index.jsx
+++ src/pages/Slider/DieCasting/Stock/index.jsx
@@ -38,161 +38,231 @@ export default function Index() {
 				accessorKey: 'name',
 				header: 'Name',
 				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
+				cell: (info) => {
+					const {
+						is_body,
+						is_puller,
+						is_cap,
+						is_link,
+						is_h_bottom,
+						is_u_top,
+						is_box_pin,
+						is_two_way_pin,
+					} = info?.row?.original;
+
+					const renderBadges = () => {
+						const badges = [
+							{
+								label: 'Body',
+								isActive: is_body === 1,
+							},
+							{
+								label: 'Puller',
+								isActive: is_puller === 1,
+							},
+							{
+								label: 'Cap',
+								isActive: is_cap === 1,
+							},
+							{
+								label: 'Link',
+								isActive: is_link === 1,
+							},
+							{
+								label: 'H Bottom',
+								isActive: is_h_bottom === 1,
+							},
+							{
+								label: 'U Top',
+								isActive: is_u_top === 1,
+							},
+							{
+								label: 'Box Pin',
+								isActive: is_box_pin === 1,
+							},
+							{
+								label: 'Two Way Pin',
+								isActive: is_two_way_pin === 1,
+							},
+						];
+
+						return badges;
+					};
+
+					return (
+						<div>
+							<span>{info.getValue()}</span>
+
+							{renderBadges().length > 0 && (
+								<div className='mt-1 flex w-max max-w-[200px] flex-wrap gap-2'>
+									{renderBadges()
+										.filter((b) => b.isActive)
+										.map((e) => (
+											<div
+												key={e.label}
+												className='badge badge-secondary badge-sm'>
+												{e.label}
+											</div>
+										))}
+								</div>
+							)}
+						</div>
+					);
+				},
 			},
+			// {
+			// 	accessorKey: 'item',
+			// 	header: 'Item',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
+
 			{
-				accessorKey: 'item',
+				accessorKey: 'item_name',
 				header: 'Item',
 				enableColumnFilter: false,
 				cell: (info) => info.getValue(),
 			},
 
-			{
-				accessorKey: 'item_name',
-				header: 'Item Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'item_short_name',
+			// 	header: 'Item Short Name',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 
-			{
-				accessorKey: 'item_short_name',
-				header: 'Item Short Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
-
-			{
-				accessorKey: 'zipper_number',
-				header: 'Zipper Number',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'zipper_number',
+			// 	header: 'Zipper Number',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 			{
 				accessorKey: 'zipper_name',
-				header: 'Zipper Name',
+				header: 'Zipper',
 				enableColumnFilter: false,
 				cell: (info) => info.getValue(),
 			},
-			{
-				accessorKey: 'zipper_short_name',
-				header: 'Zipper Short Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'zipper_short_name',
+			// 	header: 'Zipper Short Name',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 
+			// {
+			// 	accessorKey: 'end_type',
+			// 	header: 'End-Type',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 			{
-				accessorKey: 'end_type',
+				accessorKey: 'end_type_name',
 				header: 'End-Type',
 				enableColumnFilter: false,
 				cell: (info) => info.getValue(),
 			},
-			{
-				accessorKey: 'end_type_name',
-				header: 'End-Type Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
-			{
-				accessorKey: 'end_type_short_name',
-				header: 'End-Type Short Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'end_type_short_name',
+			// 	header: 'End-Type Short Name',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 
+			// {
+			// 	accessorKey: 'puller_type',
+			// 	header: 'Puller-Type',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 			{
-				accessorKey: 'puller_type',
+				accessorKey: 'puller_type_name',
 				header: 'Puller-Type',
 				enableColumnFilter: false,
 				cell: (info) => info.getValue(),
 			},
-			{
-				accessorKey: 'puller_type_name',
-				header: 'Puller-Type Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
-			{
-				accessorKey: 'puller_type_short_name',
-				header: 'Puller-Type Short Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'puller_type_short_name',
+			// 	header: 'Puller-Type Short Name',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 
+			// {
+			// 	accessorKey: 'logo_type',
+			// 	header: 'Logo-Type',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 			{
-				accessorKey: 'logo_type',
+				accessorKey: 'logo_type_name',
 				header: 'Logo-Type',
 				enableColumnFilter: false,
 				cell: (info) => info.getValue(),
 			},
-			{
-				accessorKey: 'logo_type_name',
-				header: 'Logo-Type Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
-			{
-				accessorKey: 'logo_type_short_name',
-				header: 'Logo-Type Short Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'logo_type_short_name',
+			// 	header: 'Logo-Type Short Name',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 
+			// {
+			// 	accessorKey: 'slider_body_shape',
+			// 	header: 'Body Shape',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 			{
-				accessorKey: 'slider_body_shape',
+				accessorKey: 'slider_body_shape_name',
 				header: 'Body Shape',
 				enableColumnFilter: false,
 				cell: (info) => info.getValue(),
 			},
-			{
-				accessorKey: 'slider_body_shape_name',
-				header: 'Body Shape Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
-			{
-				accessorKey: 'slider_body_shape_short_name',
-				header: 'Body Shape Short Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'slider_body_shape_short_name',
+			// 	header: 'Body Shape Short Name',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 
+			// {
+			// 	accessorKey: 'puller_link',
+			// 	header: 'Puller Link',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 			{
-				accessorKey: 'puller_link',
+				accessorKey: 'puller_link_name',
 				header: 'Puller Link',
 				enableColumnFilter: false,
 				cell: (info) => info.getValue(),
 			},
-			{
-				accessorKey: 'puller_link_name',
-				header: 'Puller Link Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
-			{
-				accessorKey: 'puller_link_short_name',
-				header: 'Puller Link Short Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'puller_link_short_name',
+			// 	header: 'Puller Link Short Name',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 
+			// {
+			// 	accessorKey: 'stopper_type',
+			// 	header: 'Stopper Type',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 			{
-				accessorKey: 'stopper_type',
+				accessorKey: 'stopper_type_name',
 				header: 'Stopper Type',
 				enableColumnFilter: false,
 				cell: (info) => info.getValue(),
 			},
-			{
-				accessorKey: 'stopper_type_name',
-				header: 'Stopper Type Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
-			{
-				accessorKey: 'stopper_type_short_name',
-				header: 'Stopper Type Short Name',
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'stopper_type_short_name',
+			// 	header: 'Stopper Type Short Name',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 
 			{
 				accessorKey: 'quantity',
@@ -236,72 +306,72 @@ export default function Index() {
 				cell: (info) => info.getValue(),
 			},
 
-			{
-				accessorKey: 'is_body',
-				header: 'Body?',
-				enableColumnFilter: false,
-				cell: (info) => (
-					<CheckboxWithoutForm checked={info.getValue()} />
-				),
-			},
+			// {
+			// 	accessorKey: 'is_body',
+			// 	header: 'Body?',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => (
+			// 		<CheckboxWithoutForm checked={info.getValue()} />
+			// 	),
+			// },
 
-			{
-				accessorKey: 'is_puller',
-				header: 'Puller?',
-				enableColumnFilter: false,
-				cell: (info) => (
-					<CheckboxWithoutForm checked={info.getValue()} />
-				),
-			},
+			// {
+			// 	accessorKey: 'is_puller',
+			// 	header: 'Puller?',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => (
+			// 		<CheckboxWithoutForm checked={info.getValue()} />
+			// 	),
+			// },
 
-			{
-				accessorKey: 'is_cap',
-				header: 'Cap?',
-				enableColumnFilter: false,
-				cell: (info) => (
-					<CheckboxWithoutForm checked={info.getValue()} />
-				),
-			},
-			{
-				accessorKey: 'is_link',
-				header: 'Link?',
-				enableColumnFilter: false,
-				cell: (info) => (
-					<CheckboxWithoutForm checked={info.getValue()} />
-				),
-			},
-			{
-				accessorKey: 'is_h_bottom',
-				header: 'H Bottom?',
-				enableColumnFilter: false,
-				cell: (info) => (
-					<CheckboxWithoutForm checked={info.getValue()} />
-				),
-			},
-			{
-				accessorKey: 'is_u_top',
-				header: 'U Top?',
-				enableColumnFilter: false,
-				cell: (info) => (
-					<CheckboxWithoutForm checked={info.getValue()} />
-				),
-			},
-			{
-				accessorKey: 'is_box_pin',
-				header: 'Box Pin?',
-				enableColumnFilter: false,
-				cell: (info) => (
-					<CheckboxWithoutForm checked={info.getValue()} />
-				),
-			},
-			{
-				accessorKey: 'is_two_way_pin',
-				header: 'Two Way Pin?',
-				enableColumnFilter: false,
-				cell: (info) => (
-					<CheckboxWithoutForm checked={info.getValue()} />
-				),
-			},
+			// {
+			// 	accessorKey: 'is_cap',
+			// 	header: 'Cap?',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => (
+			// 		<CheckboxWithoutForm checked={info.getValue()} />
+			// 	),
+			// },
+			// {
+			// 	accessorKey: 'is_link',
+			// 	header: 'Link?',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => (
+			// 		<CheckboxWithoutForm checked={info.getValue()} />
+			// 	),
+			// },
+			// {
+			// 	accessorKey: 'is_h_bottom',
+			// 	header: 'H Bottom?',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => (
+			// 		<CheckboxWithoutForm checked={info.getValue()} />
+			// 	),
+			// },
+			// {
+			// 	accessorKey: 'is_u_top',
+			// 	header: 'U Top?',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => (
+			// 		<CheckboxWithoutForm checked={info.getValue()} />
+			// 	),
+			// },
+			// {
+			// 	accessorKey: 'is_box_pin',
+			// 	header: 'Box Pin?',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => (
+			// 		<CheckboxWithoutForm checked={info.getValue()} />
+			// 	),
+			// },
+			// {
+			// 	accessorKey: 'is_two_way_pin',
+			// 	header: 'Two Way Pin?',
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => (
+			// 		<CheckboxWithoutForm checked={info.getValue()} />
+			// 	),
+			// },
 
 			{
 				accessorKey: 'actions',

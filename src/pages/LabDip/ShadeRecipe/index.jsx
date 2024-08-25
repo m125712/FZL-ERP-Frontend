ecc36e82677cc81b85661de87a36ed23120d6a1a
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2024-08-25 12:39:05 +0600

    some bug fix

--- src/pages/LabDip/ShadeRecipe/index.jsx
+++ src/pages/LabDip/ShadeRecipe/index.jsx
@@ -58,7 +58,7 @@ export default function Index() {
 				header: 'Lab Status',
 				enableColumnFilter: false,
 				cell: (info) => {
-					return info.getValue() == 'true' ? 'Done' : 'Pending';
+					return info.getValue() == 1 ? 'Done' : 'Pending';
 				},
 			},
 			{

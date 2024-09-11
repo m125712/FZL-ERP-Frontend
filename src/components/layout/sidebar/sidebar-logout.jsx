3890abc9405cb60a9c2b29a34fd81e476eae0375
alifbinhossain <alifahmed47@gmail.com>
2024-09-11 12:45:28 +0600

    ci: :sparkles: added dashboard header

--- src/components/layout/sidebar/sidebar-logout.jsx
+++ src/components/layout/sidebar/sidebar-logout.jsx
@@ -2,10 +2,13 @@ import { useAuth } from '@/context/auth';
 import { LogOut } from 'lucide-react';
 import { useLocation } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import { useNetworkState } from '@uidotdev/usehooks';
+import cn from '@/lib/cn';
 
 const SidebarLogout = () => {
 	const history = useLocation();
 	const { Logout, user } = useAuth();
+	const isOnline = useNetworkState().online;
 
 	const handleLogout = () => {
 		Logout();
@@ -15,15 +18,26 @@ const SidebarLogout = () => {
 	return (
 		<motion.button
 			whileTap={{ scale: 0.95 }}
-			className='flex w-full items-center gap-4 rounded-md bg-gradient-to-r from-accent/20 to-accent/40 px-5 py-2 text-left text-sm font-normal text-primary-content'
+			className='flex w-full items-center gap-3 rounded-md bg-gradient-to-r from-accent/10 to-accent/30 px-5 py-3 text-left text-sm font-normal text-primary-content'
 			onClick={handleLogout}>
 			<LogOut className='size-6 text-primary-content' />
 
-			<div className='flex flex-col'>
-				<span className='truncate capitalize'>{user?.name}</span>
-				<span className='text-[.6rem] capitalize text-primary-content/70'>
-					{user?.department}
-				</span>
+			<div className='flex flex-1 flex-shrink-0 items-center gap-2'>
+				<div className={cn('avatar', isOnline ? 'online' : 'offline')}>
+					<div className='size-10 rounded-full'>
+						<img
+							className='object-cover'
+							src='https://avatar.iran.liara.run/public/job/operator/male'
+						/>
+					</div>
+				</div>
+
+				<div className='flex flex-col items-start'>
+					<span className='truncate capitalize'>{user?.name}</span>
+					<span className='text-[.6rem] capitalize text-primary-content/70'>
+						{user?.department}
+					</span>
+				</div>
 			</div>
 		</motion.button>
 	);
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -3,6 +3,7 @@ import ProductionChart from './_components/BarChart/Production';
 import StatusBarChart from './_components/BarChart/Status';
 import InfoCard from './_components/Card/InfoCard';
 import { getApproval } from './_utils';
+import DashboardHeader from './_components/dashboard-header';
 
 export default function Dashboard() {
 	useEffect(() => {
@@ -10,6 +11,7 @@ export default function Dashboard() {
 	}, []);
 	return (
 		<div className='space-y-4'>
+			<DashboardHeader />
 			<div className='grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
 				<InfoCard
 					title='Nylon Metallic'

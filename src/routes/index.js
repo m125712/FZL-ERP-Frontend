a8e8628cd8950fd61119d9d6cd9a264e1c5b5ee0
Md. Shahadat Anik Sheikh <git.shahadat.anik@gmail.com>
2024-09-12 15:47:56 +0600

    FIX
    - route fixed

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,15 @@ import privateRoutes from './private';
 
 //* all private routes
 export const allPrivateRoutes = privateRoutes;
+
+//* all private routes flatten
 export const allFlatRoutes = flattenRoutes(allPrivateRoutes);
 
 //* filtered routes which has read access
-export const filteredRoutes = filterRoutes(allPrivateRoutes);
+export const filteredRoutes = filterRoutes(privateRoutes);
+
+//* flatten routes which has read access
+export const flatRoutes = flattenRoutes(filteredRoutes);
 
 //* sidebar routes which has view access only in sidebar
 export const sidebarRoutes = filterSidebarRoutes(filteredRoutes);
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,13 +2,13 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import Layout from '@/components/layout';
 
-import { filteredRoutes } from '.';
+import { flatRoutes } from '.';
 import publicRoutes from './public';
 
 export const router = createBrowserRouter([
 	...publicRoutes,
 	{
 		element: <Layout />,
-		children: filteredRoutes,
+		children: flatRoutes,
 	},
 ]);

f47914212e36563d39ffc0028a3fa9010f4436c7
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2024-09-16 19:02:21 +0600

    fix: :bug: Info LabDip bug fixed

--- src/pages/LabDip/Info/ByUUID/index.jsx
+++ src/pages/LabDip/Info/ByUUID/index.jsx
@@ -1,31 +1,36 @@
-import { useAccess, useFetch, useFetchFunc } from '@/hooks';
 import { useEffect, useState } from 'react';
+import { UseLabDipInfoByDetails } from '@/state/LabDip';
 import { Navigate, useParams } from 'react-router-dom';
+import { useAccess, useFetch, useFetchFunc } from '@/hooks';
+
+
 
 import Information from './Information';
 import Table from './Table';
+
 
 export default function Index() {
 	const { info_uuid } = useParams();
 	const haveAccess = useAccess('store__receive_by_uuid');
 
-	const { value: info, loading } = useFetch(
-		`/lab-dip/info/details/${info_uuid}`,
-		[info_uuid]
-	);
+	// const {
+	// 	value: info,
+	// 	loading,
+	// } = useFetch(`/lab-dip/info/details/${info_uuid}`, [info_uuid]);
+	const { data, isLoading } = UseLabDipInfoByDetails(info_uuid);
 
 	useEffect(() => {
 		document.title = 'Info Details';
 	}, []);
 
 	// if (!info) return <Navigate to='/not-found' />;
-	if (loading)
+	if (isLoading)
 		return <span className='loading loading-dots loading-lg z-50' />;
 
 	return (
 		<div className='space-y-2'>
-			<Information info={info} />
-			<Table {...info} />
+			<Information info={data} />
+			<Table {...data} />
 		</div>
 	);
 }

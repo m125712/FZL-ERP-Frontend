23ebc2de8bb2daaad7127814b5f1808a9a7bcacc
Shafayat <shafayat1777@gmail.com>
2024-09-24 18:55:11 +0600

    ci: :fire: Removed sfg from Metal, Vislon & Nylon

--- src/routes/Private/Vislon.jsx
+++ src/routes/Private/Vislon.jsx
@@ -1,18 +1,13 @@
 // Teeth Molding
-import VislonTeethMolding from '@/pages/Vislon/TeethMolding/RMStock';
-import VislonTeethMoldingSFG from '@/pages/Vislon/TeethMolding/SFG';
-import VislonTeethMoldingTrxLog from '@/pages/Vislon/TeethMolding/Log';
-
-// Teeth molding Production
-import VislonProduction from '@/pages/Vislon/TeethMolding/Production';
-
-// Finishing
-import VislonFinishing from '@/pages/Vislon/Finishing/RMStock';
-import VislonFinishingSFG from '@/pages/Vislon/Finishing/SFG';
 import VislonFinishingTrxLog from '@/pages/Vislon/Finishing/Log';
-
 // Finishing Production
 import VislonFinishingProduction from '@/pages/Vislon/Finishing/Production';
+// Finishing
+import VislonFinishing from '@/pages/Vislon/Finishing/RMStock';
+import VislonTeethMoldingTrxLog from '@/pages/Vislon/TeethMolding/Log';
+// Teeth molding Production
+import VislonProduction from '@/pages/Vislon/TeethMolding/Production';
+import VislonTeethMolding from '@/pages/Vislon/TeethMolding/RMStock';
 
 export const VislonRoutes = [
 	{
@@ -29,14 +24,16 @@ export const VislonRoutes = [
 						actions: ['read', 'click_name', 'click_used'],
 					},
 					{
-						name: 'SFG',
-						path: '/vislon/teeth-molding/sfg',
-						element: <VislonTeethMoldingSFG />,
-						page_name: 'vislon__teeth_molding_sfg',
+						name: 'Production',
+						path: '/vislon/teeth-molding/production',
+						element: <VislonProduction />,
+						page_name: 'vislon__teeth_molding_production',
 						actions: [
+							'create',
 							'read',
+							'update',
 							'click_production',
-							'click_to_finishing',
+							'click_transaction',
 						],
 					},
 					{
@@ -56,20 +53,6 @@ export const VislonRoutes = [
 							'click_delete_tape',
 						],
 					},
-
-					{
-						name: 'Production',
-						path: '/vislon/teeth-molding/production',
-						element: <VislonProduction />,
-						page_name: 'vislon__teeth_molding_production',
-						actions: [
-							'create',
-							'read',
-							'update',
-							'click_production',
-							'click_transaction',
-						],
-					},
 				],
 			},
 
@@ -84,13 +67,18 @@ export const VislonRoutes = [
 						actions: ['read', 'click_name', 'click_used'],
 					},
 					{
-						name: 'SFG',
-						path: '/vislon/finishing/sfg',
-						element: <VislonFinishingSFG />,
-						page_name: 'vislon__finishing_sfg',
-						actions: ['read', 'click_production'],
+						name: 'Production',
+						path: '/vislon/finishing/production',
+						element: <VislonFinishingProduction />,
+						page_name: 'vislon__finishing_production',
+						actions: [
+							'create',
+							'read',
+							'update',
+							'click_production',
+							'click_transaction',
+						],
 					},
-					// * Finishing log
 					{
 						name: 'Log',
 						path: '/vislon/finishing/log',
@@ -104,20 +92,6 @@ export const VislonRoutes = [
 							'click_delete_rm',
 							'click_update_rm_order',
 							'click_delete_rm_order',
-						],
-					},
-					// * Finishing Production
-					{
-						name: 'Production',
-						path: '/vislon/finishing/production',
-						element: <VislonFinishingProduction />,
-						page_name: 'vislon__finishing_production',
-						actions: [
-							'create',
-							'read',
-							'update',
-							'click_production',
-							'click_transaction',
 						],
 					},
 				],

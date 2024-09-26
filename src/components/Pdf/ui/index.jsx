654e351cee9fe13d9f072a55bec8af0e4dac688c
Md. Shahadat Anik Sheikh <git.shahadat.anik@gmail.com>
2024-09-26 15:14:26 +0600

    ci: :sparkles: PDF
    module added
    
    not implemented yet

--- src/components/Pdf/ui/index.jsx
+++ src/components/Pdf/ui/index.jsx
@@ -3,6 +3,8 @@ export const xMargin = 30;
 export const yMargin = 40;
 export const A4_PAGE_WIDTH = 565;
 
+export const PRIMARY_COLOR = '#27374D';
+
 // default style for all headers
 export const defaultStyle = {
 	fontSize: DEFAULT_FONT_SIZE,
@@ -11,13 +13,13 @@ export const defaultStyle = {
 // Styles
 export const styles = {
 	tableHeader: {
-		fillColor: "#ced4da",
-		color: "#000",
+		fillColor: '#ced4da',
+		color: '#000',
 	},
 	tableFooter: {
 		margin: [0, 2],
-		fillColor: "#c5c3c6",
-		color: "#000",
+		fillColor: '#c5c3c6',
+		color: '#000',
 	},
 };
 
@@ -27,6 +29,18 @@ export const TitleAndValue = (title, value) => [
 		bold: true,
 	},
 	{
-		text: `${value || "-"}\n`,
+		text: `${value || '-'}\n`,
 	},
 ];
+
+export const tableLayoutStyle = {
+	fillColor: (rowIndex, node, columnIndex) => {
+		if (rowIndex === 0) return null;
+
+		return rowIndex % 2 === 0 ? '#d1d5db' : null;
+	},
+	fillOpacity: 0.5,
+	hLineWidth: (i) => (i === 0 ? 0 : 1),
+	vLineWidth: () => 0,
+	hLineColor: (i) => (i === 1 ? PRIMARY_COLOR : '#d1d5db'),
+};
--- src/components/Pdf/utils.jsx
+++ src/components/Pdf/utils.jsx
@@ -1,8 +1,57 @@
+import { FZL_LOGO } from '@/assets/img/base64';
+
+import { defaultStyle, PRIMARY_COLOR, styles } from './ui';
+
+// * PDF DEFAULTS
+export const DEFAULT_A4_PAGE = ({ xMargin, headerHeight, footerHeight }) => ({
+	pageSize: 'A4',
+	pageOrientation: 'portrait',
+	pageMargins: [xMargin, headerHeight, xMargin, footerHeight],
+	defaultStyle,
+	styles,
+});
+
 export const company = {
-	name: "Fortune Zipper LTD.",
-	address: "Aukpara, Ashulia, Savar, DHK-1340",
-	email: "Email: info@fortunezip.com",
-	phone: "Phone: 01521533595",
-	bin: "BIN: 000537296-0403",
-	tax: "VAT: 17141000815",
+	logo: FZL_LOGO.src,
+	name: 'Fortune Zipper LTD.',
+	address: 'Aukpara, Ashulia, Savar, DHK-1340',
+	email: 'Email: info@fortunezip.com',
+	phone: 'Phone: 01521533595',
+	bin: 'BIN: 000537296-0403',
+	tax: 'VAT: 17141000815',
+};
+
+export const getTable = (
+	field,
+	name,
+	alignment = 'left',
+	headerStyle = 'tableHeader',
+	cellStyle = 'tableCell'
+) => ({
+	field,
+	name,
+	alignment,
+	headerStyle,
+	cellStyle,
+});
+
+export const TableHeader = (node) => {
+	return [
+		...node.map((nodeItem) => ({
+			text: nodeItem.name,
+			// style: nodeItem.headerStyle,
+			alignment: nodeItem.alignment,
+			color: PRIMARY_COLOR,
+			bold: true,
+		})),
+	];
+};
+
+// * HELPER: GET EMPTY COLUMN
+const getEmptyColumn = (colSpan) => {
+	const EMPTY_COLUMN = [];
+	for (let i = 0; i < colSpan - 1; i++) {
+		EMPTY_COLUMN.push('');
+	}
+	return EMPTY_COLUMN;
 };

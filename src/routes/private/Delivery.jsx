695d0f19bfab74b56077e8c1add3470d088d49dd
Md. Shahadat Anik Sheikh <git.shahadat.anik@gmail.com>
2024-12-13 19:18:09 +0600

    style: :art: page route changes

--- src/routes/private/Delivery.jsx
+++ src/routes/private/Delivery.jsx
@@ -39,7 +39,86 @@ export const DeliveryRoutes = [
 				page_name: 'delivery__rm',
 				actions: ['create', 'read', 'used', 'delete'],
 			},
-
+			// * Gate Pass
+			{
+				name: 'Gate Pass',
+				path: '/delivery/gate-pass',
+				element: <GatePass />,
+				page_name: 'delivery__gate_pass',
+				actions: ['create', 'read'],
+			},
+			// * Challan
+			{
+				name: 'Challan',
+				path: '/delivery/challan',
+				element: <Challan />,
+				page_name: 'delivery__challan',
+				actions: [
+					'create',
+					'read',
+					'update',
+					'delete',
+					'click_gate_pass',
+					'click_receive_status',
+					'click_gate_pass_override',
+					'click_receive_status_override',
+				],
+			},
+			{
+				name: 'Challan By Date',
+				path: '/delivery/challan-by-date/:date',
+				element: <ChallanByDate />,
+				page_name: 'delivery__challan_by_date',
+				actions: [
+					'create',
+					'read',
+					'update',
+					'delete',
+					'click_gate_pass',
+					'click_receive_status',
+					'click_gate_pass_override',
+					'click_receive_status_override',
+				],
+				hidden: true,
+			},
+			{
+				name: 'Challan Details',
+				path: '/delivery/challan/:uuid',
+				element: <ChallanDetails />,
+				page_name: 'delivery__challan_details',
+				actions: [
+					'create',
+					'read',
+					'update',
+					'delete',
+					'click_receive_status',
+				],
+				hidden: true,
+			},
+			{
+				name: 'Challan Entry',
+				path: '/delivery/challan/entry',
+				element: <ChallanEntry />,
+				page_name: 'delivery__challan_entry',
+				actions: ['create', 'read', 'update', 'delete'],
+				hidden: true,
+			},
+			{
+				name: 'Challan Update',
+				path: '/delivery/challan/:uuid/update',
+				element: <ChallanEntry />,
+				page_name: 'delivery__challan_update',
+				actions: ['create', 'read', 'update', 'delete'],
+				hidden: true,
+			},
+			// * Warehouse Receive
+			{
+				name: 'Warehouse Receive',
+				path: '/delivery/warehouse-receive',
+				element: <WarehouseRecv />,
+				page_name: 'delivery__warehouse_recv',
+				actions: ['create', 'read'],
+			},
 			// *Packing List
 			{
 				name: 'Packing List',
@@ -101,143 +180,7 @@ export const DeliveryRoutes = [
 
 				hidden: true,
 			},
-			// * Warehouse Receive
-			{
-				name: 'Warehouse Receive',
-				path: '/delivery/warehouse-receive',
-				element: <WarehouseRecv />,
-				page_name: 'delivery__warehouse_recv',
-				actions: ['create', 'read'],
-			},
-			// * Challan
-			{
-				name: 'Challan',
-				path: '/delivery/challan',
-				element: <Challan />,
-				page_name: 'delivery__challan',
-				actions: [
-					'create',
-					'read',
-					'update',
-					'delete',
-					'click_gate_pass',
-					'click_receive_status',
-					'click_gate_pass_override',
-					'click_receive_status_override',
-				],
-			},
-			// *
-			{
-				name: 'Challan By Date',
-				path: '/delivery/challan-by-date/:date',
-				element: <ChallanByDate />,
-				page_name: 'delivery__challan_by_date',
-				actions: [
-					'create',
-					'read',
-					'update',
-					'delete',
-					'click_gate_pass',
-					'click_receive_status',
-					'click_gate_pass_override',
-					'click_receive_status_override',
-				],
-				hidden: true,
-			},
-			{
-				name: 'Challan Details',
-				path: '/delivery/challan/:uuid',
-				element: <ChallanDetails />,
-				page_name: 'delivery__challan_details',
-				actions: [
-					'create',
-					'read',
-					'update',
-					'delete',
-					'click_receive_status',
-				],
-				hidden: true,
-			},
-			{
-				name: 'Challan Entry',
-				path: '/delivery/challan/entry',
-				element: <ChallanEntry />,
-				page_name: 'delivery__challan_entry',
-				actions: ['create', 'read', 'update', 'delete'],
-				hidden: true,
-			},
-			{
-				name: 'Challan Update',
-				path: '/delivery/challan/:uuid/update',
-				element: <ChallanEntry />,
-				page_name: 'delivery__challan_update',
-				actions: ['create', 'read', 'update', 'delete'],
-				hidden: true,
-			},
-			// * Gate Pass
-			{
-				name: 'Gate Pass',
-				path: '/delivery/gate-pass',
-				element: <GatePass />,
-				page_name: 'delivery__gate_pass',
-				actions: ['create', 'read'],
-			},
-			// * Thread Challan
-			// {
-			// 	name: 'Thread Challan',
-			// 	path: '/thread/challan',
-			// 	element: <ThreadChallan />,
-			// 	page_name: 'thread__challan',
-			// 	actions: [
-			// 		'create',
-			// 		'read',
-			// 		'update',
-			// 		'delete',
-			// 		'click_gate_pass',
-			// 		'click_receive_status',
-			// 		'click_gate_pass_override',
-			// 		'click_receive_status_override',
-			// 	],
-			// },
-			// {
-			// 	name: 'Thread Challan Details',
-			// 	path: '/thread/challan/:uuid',
-			// 	element: <ThreadChallanDetails />,
-			// 	page_name: 'thread__challan_details',
-			// 	actions: ['create', 'read', 'update', 'delete'],
-			// 	hidden: true,
-			// },
-			// {
-			// 	name: 'Thread Challan Entry',
-			// 	path: '/thread/challan/entry',
-			// 	element: <ThreadChallanEntry />,
-			// 	page_name: 'thread__challan_entry',
-			// 	actions: ['create', 'read', 'update', 'delete'],
-			// 	hidden: true,
-			// },
-			// {
-			// 	name: 'Thread Challan Update',
-			// 	path: '/thread/challan/:challan_uuid/update',
-			// 	element: <ThreadChallanEntry />,
-			// 	page_name: 'thread__challan_update',
-			// 	actions: ['create', 'read', 'update', 'delete'],
-			// 	hidden: true,
-			// },
-			// * Log
-			{
-				name: 'Log',
-				path: '/delivery/log',
-				element: <Log />,
-				page_name: 'delivery__log',
-				actions: [
-					'create',
-					'read',
-					'update',
-					'delete',
-					'click_update_rm_order',
-					'click_delete_rm_order',
-				],
-			},
+
 			// * Delivery Carton
 			{
 				name: 'Carton',
@@ -269,6 +212,22 @@ export const DeliveryRoutes = [
 					'click_active_override',
 				],
 			},
+			// * Log
+			{
+				name: 'Log',
+				path: '/delivery/log',
+				element: <Log />,
+				page_name: 'delivery__log',
+				actions: [
+					'create',
+					'read',
+					'update',
+					'delete',
+					'click_update_rm_order',
+					'click_delete_rm_order',
+				],
+			},
+
 			// //* Test
 			// {
 			// 	name: 'Test',

6a0456416cb09e1e866c838b5840aba44b3baee4
Md. Shahadat Anik Sheikh <git.shahadat.anik@gmail.com>
2024-12-23 13:28:01 +0600

    fix: :fire: Planning Balance added

--- src/pages/Planning/FinishingBatch/index.jsx
+++ src/pages/Planning/FinishingBatch/index.jsx
@@ -88,6 +88,16 @@ export default function index() {
 				cell: (info) => info.getValue(),
 			},
 			{
+				// accessorKey: 'total_batch_production_quantity',
+				accessorFn: (row) =>
+					row.total_batch_quantity -
+					row.total_batch_production_quantity,
+				id: 'balance',
+				header: 'Balance',
+				enableColumnFilter: false,
+				cell: (info) => info.getValue(),
+			},
+			{
 				accessorKey: 'production_date',
 				header: (
 					<div className='flex flex-col'>
@@ -106,17 +116,17 @@ export default function index() {
 				enableColumnFilter: false,
 				cell: (info) => info.getValue(),
 			},
-			{
-				accessorKey: 'slider_lead_time',
-				header: (
-					<div>
-						Finishing Slider <br />
-						Lead Time
-					</div>
-				),
-				enableColumnFilter: false,
-				cell: (info) => info.getValue(),
-			},
+			// {
+			// 	accessorKey: 'slider_lead_time',
+			// 	header: (
+			// 		<div>
+			// 			Finishing Slider <br />
+			// 			Lead Time
+			// 		</div>
+			// 	),
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => info.getValue(),
+			// },
 			{
 				accessorFn: (row) => {
 					const { production_date, slider_lead_time } = row;
@@ -135,7 +145,7 @@ export default function index() {
 				header: (
 					<div>
 						Remaining Date <br />
-						Slider
+						#Slider
 					</div>
 				),
 				enableColumnFilter: false,
@@ -157,23 +167,23 @@ export default function index() {
 					);
 				},
 			},
-			{
-				accessorKey: 'dyeing_lead_time',
-				header: (
-					<div>
-						Finishing Dyeing <br />
-						Lead Time
-					</div>
-				),
-				enableColumnFilter: false,
-				cell: (info) => (info.getValue() ? info.getValue() : '---'),
-			},
+			// {
+			// 	accessorKey: 'dyeing_lead_time',
+			// 	header: (
+			// 		<div>
+			// 			Finishing Dyeing <br />
+			// 			Lead Time
+			// 		</div>
+			// 	),
+			// 	enableColumnFilter: false,
+			// 	cell: (info) => (info.getValue() ? info.getValue() : '---'),
+			// },
 			{
 				accessorKey: 'remaining_dyeing_lead_time',
 				header: (
 					<div>
 						Remaining Date <br />
-						Dyeing
+						#Dyeing
 					</div>
 				),
 				enableColumnFilter: false,

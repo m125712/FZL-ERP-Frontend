7fd667f17a48ade119f7b63394b368b3358ba261
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-01-06 18:23:44 +0600

    refactor: refactor PDF Thread Packing  List Sticker

--- src/components/Pdf/ThreadPackeListSticker/index.jsx
+++ src/components/Pdf/ThreadPackeListSticker/index.jsx
@@ -8,7 +8,6 @@ import { generateBarcodeAsBase64 } from './Barcode';
 import { getPageFooter } from './utils';
 
 const getDateFormate = (date) => format(new Date(date), 'dd/MM/yy');
-
 export default function Index(data) {
 	const shade = new Set();
 	const subStreat = new Set();
@@ -17,7 +16,6 @@ export default function Index(data) {
 	const color = new Set();
 	const style = new Set();
 	let totalQuantity = 0;
-	const conePerCarton = data?.packing_list_entry[0].cone_per_carton;
 
 	data?.packing_list_entry?.forEach((item) => {
 		shade.add(item.recipe_name);
@@ -29,17 +27,26 @@ export default function Index(data) {
 		totalQuantity += parseInt(item.quantity, 10) || 0;
 	});
 
-	const cartonNumber = Math.ceil(
-		totalQuantity / data?.packing_list_entry[0].cone_per_carton
-	);
+	const pdfDocGenerator = pdfMake.createPdf({
+		...CUSTOM_PAGE_THREAD_STICKER({
+			xMargin: 5,
+			headerHeight: 5,
+			footerHeight: 60,
+		}),
 
-	const content = [];
+		// Page Footer
+		footer: (currentPage, pageCount) => ({
+			table: getPageFooter({
+				currentPage,
+				pageCount,
+				rank: data?.packing_list_wise_rank,
+				packing_number: data?.packing_number,
+				data: data,
+			}),
+		}),
 
-	for (let index = 0; index < cartonNumber; index++) {
-		let numberOfCone =
-			totalQuantity > conePerCarton ? conePerCarton : totalQuantity;
-		totalQuantity -= conePerCarton;
-		content.push(
+		// * Main Table
+		content: [
 			{
 				margin: [0, 92, 0, 0],
 				image: generateBarcodeAsBase64(
@@ -47,9 +54,11 @@ export default function Index(data) {
 					data?.uuid
 				),
 				width: 200,
+				// height: 20,
 				alignment: 'center',
 				colSpan: 6,
 			},
+
 			{
 				table: {
 					widths: ['*', '*', '*'],
@@ -73,6 +82,7 @@ export default function Index(data) {
 								text: data?.order_number,
 								bold: true,
 							},
+
 							{
 								text: `${Array.from(color).join(', ')}`,
 								bold: true,
@@ -80,13 +90,9 @@ export default function Index(data) {
 							},
 							{},
 						],
+
 						[
-							{
-								text:
-									data?.packing_number +
-									`(${index + 1}/${cartonNumber})`,
-								bold: true,
-							},
+							{ text: data?.packing_number, bold: true },
 							{
 								text: `${Array.from(shade).join(', ')}`,
 								bold: true,
@@ -102,7 +108,7 @@ export default function Index(data) {
 							},
 							{},
 							{
-								text: `${numberOfCone} Cone`,
+								text: `${totalQuantity} Cone`,
 								bold: true,
 								fontSize: DEFAULT_FONT_SIZE + 1,
 							},
@@ -119,34 +125,10 @@ export default function Index(data) {
 						],
 					],
 				},
+
 				layout: 'noBorders',
-			}
-		);
-
-		if (index < cartonNumber - 1) {
-			content.push({ text: '', pageBreak: 'after' });
-		}
-	}
-
-	const pdfDocGenerator = pdfMake.createPdf({
-		...CUSTOM_PAGE_THREAD_STICKER({
-			xMargin: 5,
-			headerHeight: 5,
-			footerHeight: 60,
-		}),
-
-		// Page Footer
-		footer: (currentPage, pageCount) => ({
-			table: getPageFooter({
-				currentPage,
-				pageCount,
-				rank: data?.packing_list_wise_rank,
-				packing_number: data?.packing_number,
-				data: data,
-			}),
-		}),
-
-		content: content,
+			},
+		],
 	});
 
 	return pdfDocGenerator;

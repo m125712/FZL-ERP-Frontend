fcbbaba8f1a88aa90ba6ac28f2cf836bf6df56a2
Shafayat <shafayat1777@gmail.com>
2025-01-06 11:31:25 +0600

    ci: :bug: Slider/Coloring/production: removed u-top, h-bottom & box-pin requirement

--- src/pages/Slider/Coloring/Production/Production.jsx
+++ src/pages/Slider/Coloring/Production/Production.jsx
@@ -37,19 +37,21 @@ export default function Index({
 	const { invalidateQuery } = useSliderColoringProduction();
 	const { user } = useAuth();
 
-	const MAX_PROD_KG =
-		updateSliderProd?.order_type === 'slider'
-			? updateSliderProd.coloring_stock
-			: Math.floor(
-					Math.min(
-						Number(updateSliderProd?.u_top_quantity) / 2,
-						updateSliderProd.coloring_stock,
-						updateSliderProd.end_type_name === 'Close End'
-							? updateSliderProd.h_bottom_quantity
-							: updateSliderProd.box_pin_quantity
-					)
-				);
+	// const MAX_PROD_KG =
+	// 	updateSliderProd?.order_type === 'slider'
+	// 		? updateSliderProd.coloring_stock
+	// 		: Math.floor(
+	// 				Math.min(
+	// 					Number(updateSliderProd?.u_top_quantity) / 2,
+	// 					updateSliderProd.coloring_stock,
+	// 					updateSliderProd.end_type_name === 'Close End'
+	// 						? updateSliderProd.h_bottom_quantity
+	// 						: updateSliderProd.box_pin_quantity
+	// 				)
+	// 			);
 
+	const MAX_PROD_KG = updateSliderProd?.coloring_stock;
+	
 	const { register, handleSubmit, errors, reset, watch, control, context } =
 		useRHF(
 			{

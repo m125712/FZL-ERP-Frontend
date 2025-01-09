3eabc2f36db0dd916eeeb30147fffc59d26ef159
Md. Shahadat Anik Sheikh <git.shahadat.anik@gmail.com>
2025-01-09 12:21:33 +0600

    fix: update tape calculation to handle inch units and simplify total size logic

--- src/util/GetRequiredTapeKg.jsx
+++ src/util/GetRequiredTapeKg.jsx
@@ -11,19 +11,29 @@ export const getRequiredTapeKg = ({
 		dyed_mtr_per_kg,
 		order_quantity,
 		order_type,
+		unit,
 	} = row;
 
+	const isInch = unit?.toLowerCase() === 'inch';
 	const mtr_per_kg = type === 'raw' ? raw_mtr_per_kg : dyed_mtr_per_kg;
 
-	let total_size = 0,
-		total_size_in_mtr = 0;
+	let total_size_in_mtr = 0;
 
 	if (order_type === 'tape') {
 		const give = input_quantity !== null ? input_quantity : size;
+
 		total_size_in_mtr = parseFloat(give); // * the size is given in MTR
 	} else {
-		total_size = parseFloat(top) + parseFloat(bottom) + parseFloat(size);
 		const give = input_quantity !== null ? input_quantity : order_quantity;
+		const top_bottom = parseFloat(top) + parseFloat(bottom);
+		let total_size = 0;
+
+		if (isInch) {
+			total_size = top_bottom + parseFloat(size) * 2.54;
+		} else {
+			total_size = top_bottom + parseFloat(size);
+		}
+
 		total_size_in_mtr = (total_size * parseFloat(give)) / 100;
 	}
 

2b3dab4e40de755072df9b461f8b13acf366b722
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-04-22 13:22:21 +0600

    fix: Adjust table column widths and colspan for packing list PDF generation

--- src/components/Pdf/ThreadPackeListStickerV2/index.jsx
+++ src/components/Pdf/ThreadPackeListStickerV2/index.jsx
@@ -58,17 +58,20 @@ export default function Index(data) {
 			},
 			{
 				table: {
-					widths: ['*', '*', '*'],
+					widths: [17, 30, 90, 40],
 					body: [
 						[
 							{
 								text: Array.from(subStreat).join(', '),
 								bold: true,
 							},
+
 							{
 								text: Array.from(count).join(', '),
 								bold: true,
+								colSpan: 2,
 							},
+							{},
 							{
 								text: getDateFormate(data?.created_at),
 								bold: true,
@@ -78,24 +81,27 @@ export default function Index(data) {
 							{
 								text: data?.order_number,
 								bold: true,
-							},
-							{
-								text: `${Array.from(color).join(', ')}`,
-								bold: true,
 								colSpan: 2,
 							},
 							{},
-						],
-
-						[
 							{
-								text: Array.from(length).join(', ') + ' M',
+								text: `${Array.from(color).join(', ')}`,
 								bold: true,
 							},
 							{
 								text: `${Array.from(shade).join(', ')}`,
 								bold: true,
 							},
+						],
+
+						[
+							{
+								text: Array.from(length).join(', ') + ' M',
+								bold: true,
+								colSpan: 3,
+							},
+							{},
+							{},
 							{
 								text: `${numberOfCone} Cone`,
 								bold: true,
@@ -106,9 +112,10 @@ export default function Index(data) {
 							{
 								text: `${Array.from(style).join(', ')}`,
 								bold: true,
-								colSpan: 3,
+								colSpan: 4,
 								fontSize: DEFAULT_FONT_SIZE - 1,
 							},
+							{},
 							{},
 							{},
 						],

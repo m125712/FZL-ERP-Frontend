971b9557e8666cb1ef1d0a1d51848c4156a8be68
Mirsad Al Hossain <51824780+m125712@users.noreply.github.com>
2025-06-15 12:48:19 +0600

    fix:Front Custom VFS file added

--- src/components/Pdf/index.jsx
+++ src/components/Pdf/index.jsx
@@ -1,14 +1,15 @@
 import pdfMake from 'pdfmake/build/pdfmake';
 
-const BASE_URL = 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts';
-const ROBOTO = BASE_URL + '/Roboto/Roboto-';
+import { vfs } from './vfs_fonts';
+
+pdfMake.vfs = vfs;
 
 pdfMake.fonts = {
 	Roboto: {
-		normal: ROBOTO + 'Regular.ttf',
-		bold: ROBOTO + 'Medium.ttf',
-		italics: ROBOTO + 'Italic.ttf',
-		bolditalics: ROBOTO + 'MediumItalic.ttf',
+		normal: 'Roboto-Regular.ttf',
+		bold: 'Roboto-Bold.ttf',
+		italics: 'Roboto-Italic.ttf',
+		bolditalics: 'Roboto-BoldItalic.ttf',
 	},
 };
 

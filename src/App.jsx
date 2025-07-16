b5dfa96e8ef31ce96ab29132b05788036e89c268
shafayat <shafayat1777@gmail.com>
2025-07-16 19:38:28 +0600

    1st iteration of notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { router } from '@/routes/router';
 import AuthProvider from '@context/auth';
 import { RouterProvider } from 'react-router/dom';
+import { toast } from 'react-toastify';
 
 import { Toast } from '@/components/Toast';
 
 import { Loader } from './components/Feedback';
+import NotificationSocket from './util/Notification';
 
 function App() {
+	const handleNewIssue = (issue) => {
+		toast.info(`🚨 New issue reported: ${issue}`);
+	};
 	return (
 		<AuthProvider>
 			<RouterProvider router={router} fallbackElement={<Loader />} />
 			<Toast />
+			<NotificationSocket onNewIssue={handleNewIssue} />
 		</AuthProvider>
 	);
 }
---
+++ src/util/Notification.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+const socket = io('http://103.147.163.46:4010'); // adjust URL
+
+export default function NotificationSocket({ onNewIssue }) {
+	useEffect(() => {
+		socket.on('new-issue', onNewIssue);
+		return () => {
+			socket.off('new-issue', onNewIssue);
+		};
+	}, [onNewIssue]);
+
+	return null; // No UI, just a listener
+}

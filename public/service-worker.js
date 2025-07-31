bf345f9a5c7e27b96b5d2c85bc81c6e4e0359d5d
Shafayat <shafayat1777@gmail.com>
2025-07-31 12:57:35 +0600

    ci: :sparkles: Added redirect link to notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,38 @@
-// This code runs when a push is received, even if your site is closed.
 self.addEventListener('push', function (event) {
 	const data = event.data.json();
 	// console.log('[Service Worker] Push Received:', data);
 
 	const options = {
 		body: data.body,
-		icon: '✅', // Optional: an icon to show with the notification
+		icon: '✅',
+		data: {
+			url: data.url, // ✅ Relative URL
+		},
 	};
 
-	// Show the notification
 	event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+	// console.log('Notification click received');
+	event.notification.close();
+
+	const targetUrl = event.notification.data.url;
+
+	event.waitUntil(
+		clients
+			.matchAll({ type: 'window', includeUncontrolled: true })
+			.then((clientList) => {
+				// If any open window matches the same origin, focus it
+				for (const client of clientList) {
+					if (client.url === targetUrl && 'focus' in client) {
+						return client.focus();
+					}
+				}
+				// Or just open a new tab
+				if (clients.openWindow) {
+					return clients.openWindow(targetUrl);
+				}
+			})
+	);
 });
